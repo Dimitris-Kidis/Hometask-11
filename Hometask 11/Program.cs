4898260e739d9dc7e57e71ac8929a4a8ea9e6c88
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-02 15:42:10 +0300

    Add project files.

---
+++ Hometask 11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// 1. Create one or more sequences and use one operator of each type:
+//     - filtering
+//     - projection
+//     - joining
+//     - ordering
+//     - grouping
+//     - set operators
+//     - conversion methods
+//     - element operators
+//     - aggregation methods
+//     - quantifiers
+//     - generation methods
+// 2. Use closures 
+
+namespace App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
